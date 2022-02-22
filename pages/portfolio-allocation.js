05520ce826db3893ab6ddde03b62c84a6bb1ee13
Lucas Parzych <hi.lucas.p@gmail.com>
2022-02-22 18:46:35 -0500

    toggle value col

--- pages/portfolio-allocation.js
+++ pages/portfolio-allocation.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import currency from "currency.js";
 import { base, tables } from "../lib/node/airtable";
+import { useState } from "react";
 const { reduceBy, sort, descend, map } = require("ramda");
 
 export async function getServerSideProps() {
@@ -31,6 +32,13 @@ export default function PortfolioAllocation({ percentages, values }) {
   const tokensSortedByPercentDesc = sort(
     descend((token) => percentages[token])
   )(Object.keys(percentages));
+
+  const [showValue, setShowValue] = useState(true);
+
+  const toggleShowValue = () => {
+    setShowValue((v) => !v);
+  };
+
   return (
     <div>
       <Head>
@@ -49,7 +57,7 @@ export default function PortfolioAllocation({ percentages, values }) {
           <thead>
             <tr>
               <th>Token&emsp;</th>
-              <th>Value (USD)</th>
+              {showValue && <th>Value (USD)</th>}
               <th>Percent</th>
             </tr>
           </thead>
@@ -57,12 +65,18 @@ export default function PortfolioAllocation({ percentages, values }) {
             {tokensSortedByPercentDesc.map((token) => (
               <tr key={token}>
                 <td>{token}&emsp;</td>
-                <td>{`${currency(values[token]).format(true)}`}</td>
+                {showValue && (
+                  <td>{`${currency(values[token]).format(true)}`}</td>
+                )}
                 <td>{percentages[token].toFixed(1)}</td>
               </tr>
             ))}
           </tbody>
         </table>
+        <br />
+        <button onClick={toggleShowValue}>
+          {showValue ? "Hide Value Column" : "Show Value Column"}
+        </button>
       </main>
     </div>
   );
